730e39c66eb82bb29acfca9daa909ffcd17c9257
david <judagosa8520@gmail.com>
2024-04-13 17:35:18 -0500

    refactor: App component to use Redux store for counter state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementBy2 } from './store/slices/counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -15,11 +18,18 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <p>count is: {counter}</p>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementBy2(2))}>
+          Increment by 2
         </button>
       </div>
     </>
