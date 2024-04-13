0cf6d34b1b36d652db69501b58b2042a3c6bfa32
david <judagosa8520@gmail.com>
2024-04-13 17:34:58 -0500

    feat: add decrement and incrementBy2 reducers to counterSlice

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    counter: 10
+    counter: 10,
   },
   reducers: {
     increment: (state) => {
@@ -13,8 +13,14 @@ export const counterSlice = createSlice({
       // immutable state based off those changes
       state.counter += 1;
     },
-  },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
+    incrementBy2: (state, action) => {
+      state.counter += action.payload;
+    },
+  }
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions
+export const { increment, decrement, incrementBy2 } = counterSlice.actions
