462fd8c7d577b1bd0addef22bcc04fa398cdd173
david <judagosa8520@gmail.com>
2024-04-14 19:59:02 -0500

    refactor: main.jsx to use TodoApp component for Todo state management

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { store } from './store'
 import { Provider } from 'react-redux'
-import { PokemonApp } from './PokemonApp.jsx'
+// import { PokemonApp } from './PokemonApp.jsx'
+import { TodoApp } from './TodoApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>,
 )
